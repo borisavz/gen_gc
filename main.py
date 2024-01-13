6184d4707089defd2452813ba559cb3fe8cda9d4
borisavz <borisavzivanovic@gmail.com>
2024-01-13 20:12:57 +0100

    Second generation GC

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ class Object:
 class Generation:
     def __init__(self):
         self.objects = defaultdict(lambda: None)
+
+        # Deviates from spec: list of objects that are pointed to
         self.remembered_set = set()
 
 
@@ -30,34 +32,58 @@ third_gen = Generation()
 
 
 def first_gen_collect():
-    root_set = stack_refs.copy()
+    root_set = stack_refs.union(first_gen.remembered_set)
 
-    #TODO: get objects from remembered set and scan their references
-
-    mark_first_gen_recursive(root_set)
+    mark_recursive(root_set, first_gen.objects)
 
     for v in first_gen.objects.values():
         if v.reached:
+            v.reached = False
             second_gen.objects[v.id] = v
 
     first_gen.objects = defaultdict(lambda: None)
 
 
-def mark_first_gen_recursive(references):
-    for r in references:
-        obj = first_gen.objects[r]
-
-        if not obj.reached:
-            obj.reached = True
-            mark_first_gen_recursive(obj.references)
-
-
 def second_gen_collect():
-    pass
+    root_set = stack_refs.union(
+        first_gen.objects,
+        first_gen.remembered_set,
+        second_gen.objects,
+        second_gen.remembered_set
+    )
+
+    objects = {**first_gen.objects, **second_gen.objects}
+
+    mark_recursive(root_set, objects)
+
+    for v in second_gen.objects.values():
+        if v.reached:
+            v.reached = False
+            third_gen.objects[v.id] = v
+
+    second_gen.objects = defaultdict(lambda: None)
+
+    for v in first_gen.objects.values():
+        if v.reached:
+            v.reached = False
+            second_gen.objects[v.id] = v
+
+    first_gen.objects = defaultdict(lambda: None)
 
 
 def third_gen_collect():
     pass
+
+def mark_recursive(references, objects):
+    for r in references:
+        obj = objects[r]
+
+        if obj is None:
+            continue
+
+        if not obj.reached:
+            obj.reached = True
+            mark_recursive(obj.references, objects)
 
 
 a = Object('a')
@@ -75,5 +101,14 @@ first_gen.objects[c.id] = c
 a.remove_reference(b)
 
 first_gen_collect()
+
+print(first_gen)
+
+d = Object('d')
+
+stack_refs.add(d.id)
+first_gen.objects[d.id] = d
+
+second_gen_collect()
 
 print(first_gen)
